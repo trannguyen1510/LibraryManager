51f2b22e5b34e3689228df692de7d95587803aa9
NguyenSSS <44703232+trannguyen1510@users.noreply.github.com>
2021-07-04 12:48:24 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace LibraryManager
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Main());
+            Application.Run(new Login());
         }
     }
 }
